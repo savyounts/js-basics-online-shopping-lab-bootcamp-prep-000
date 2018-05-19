947934f16a9061933d46136ea7f8d0062ffda7fc
Sav Younts <savyounts@gmail.com>
2018-05-19 16:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,5 +62,12 @@ function removeFromCart(itemName) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+if(cardNumber) {
+  for(var i=0; i<cart.length; i++){
+    cart.splice(i,1)
+  }
+  return 'Your total cost is ' +total() +', which will be charged to the card ' + cardNumber
+}else {
+}
+  
 }
